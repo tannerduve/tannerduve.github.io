c68f68d12baa78eb6df5e035ec438c9e97810192
tannerduve <122851174+tannerduve@users.noreply.github.com>
2025-11-18 18:50:13 +0000

    Deploying to gh-pages from @ tannerduve/tannerduve.github.io@fdd11d3bc6f811396754c0725b2264c9ded0ebc5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/al-folio/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -37,12 +37,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/repositories/";
           },
+        },{id: "nav-coursework",
+          title: "Coursework",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/coursework/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/cv/";
+          },
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/talks/";
           },
         },{id: "nav-teaching",
           title: "teaching",
